07823cc1113192f0da334f643f5c915b6c74330f
okamoto <okamoto_yu-ki@outlook.jp>
2021-08-12 11:32:21 +0900

    ホストアドレス、ポートを環境変数で設定可能に変更

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
+use std::env;
+
 use actix_web::{
     middleware, web, App, HttpServer,
 };
+use dotenv::dotenv;
+use listenfd::ListenFd;
 
 use todo_api::routes::todo;
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
             .service(web::scope("/todo").configure(todo::todos))
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    });
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Set host in .env");
+            let port = env::var("PORT").expect("Set port in .env");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+    server.run().await
 }
